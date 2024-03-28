9407f1503fb6cf85ff74d5f714de95841aaa4501
Rafael Motta <rafaelm.programador@gmail.com>
2024-03-28 15:20:34 -0300

    Criação da section de tabs

--- scripts/home.js
+++ scripts/home.js
@@ -9,3 +9,27 @@ function hideSidebar() {
   const sidebar = document.querySelector(".navItensContainerMobile");
   sidebar.style.display = "none";
 }
+
+/* Vertical Tabs  */
+
+function openVerticalTabs(e, tabsId) {
+  let i, optionsTabContent, optionsNavItem;
+
+  optionsTabContent = document.getElementsByClassName("optionsTabContent");
+  optionsNavItem = document.getElementsByClassName("optionsNavItem");
+
+  for (i = 0; i < optionsTabContent.length; i++) {
+    optionsTabContent[i].style.display = "none";
+  }
+
+  for (i = 0; i < optionsNavItem.length; i++) {
+    optionsNavItem[i].className = optionsNavItem[i].className.replace(
+      " active",
+      ""
+    );
+  }
+
+  document.getElementById(tabsId).style.display = "block";
+  e.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
