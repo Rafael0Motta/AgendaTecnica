e087a53db674d1ce2203cea41b4abe4f2fc03bf3
Rafael Motta <rafaelm.programador@gmail.com>
2024-03-28 15:09:54 -0300

    Criação do menu de navegação da home  do projeto, bem como sua responsividade, estilização e logica

---
+++ scripts/home.js
@@ -0,0 +1,11 @@
+/* Hamburguer Menu */
+
+function showSidebar() {
+  const sidebar = document.querySelector(".navItensContainerMobile");
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".navItensContainerMobile");
+  sidebar.style.display = "none";
+}
