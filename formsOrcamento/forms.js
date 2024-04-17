f28040f4e92b9474d75e79a64916e566eaef3ddb
Rafael Motta <149005855+Rafael0Motta@users.noreply.github.com>
2024-04-17 16:32:45 -0300

    Add files via upload

--- formsOrcamento/forms.js
+++ formsOrcamento/forms.js
@@ -1,3 +1,26 @@
+/* SCROLL BUTTON */
+window.addEventListener("scroll", function () {
+  var topBtn = document.getElementById("topBtn");
+  if (window.scrollY > 100) {
+    topBtn.classList.add("active");
+  } else {
+    topBtn.classList.remove("active");
+  }
+});
+
+/* HAMBURGUER MENU */
+function showSidebar() {
+  const sidebar = document.querySelector(".navItensContainerMobile");
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".navItensContainerMobile");
+  sidebar.style.display = "none";
+}
+
+/* FORMS */
+
 function handleCheckboxChange() {
   let outrosCheckbox = document.getElementById("Outros");
   let textarea = document.getElementById("mensagem");
